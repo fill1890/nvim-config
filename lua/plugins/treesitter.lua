ed7fa09a18442912724ce92baea30ec231db852a
Andrew Walls <andrew.walls@kazrobotics.com.au>
2024-06-06 10:53:17 +1000

    fix: update treesitter on run

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,10 @@ local M = {}
 function M.setup(use)
     use {
         'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true})
+            ts_update()
+        end,
         config = function()
             require'nvim-treesitter.configs'.setup {
                 ensure_installed = {
